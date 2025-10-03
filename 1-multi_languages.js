8867124054fdcc91b3ccc7948fa4b59bc389d5ce
Ntsana Pheko <ntsanapheko@gmail.com>
2025-10-03 11:51:17 +0200

    added 1-multi_languages.js file

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+const firstLine = 'C is fun'; 
+console.log(firstLine); 
+
+const secondLine = 'Python is cool'; 
+console.log(secondLine);
+
+const thirdLine = 'JavaScript is amazing'; 
+console.log(thirdLine); 
+
