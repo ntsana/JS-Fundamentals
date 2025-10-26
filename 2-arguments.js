ea57b9d0c965d6f6de1894b643f9276fe5dd97b5
Ntsana Pheko <ntsanapheko@gmail.com>
2025-10-26 20:40:54 +0200

    updates

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,11 @@
-if (!process.argv[2]) {
-  console.log("no argument");
-} else if(!process.argv[3]){
-  console.log("Argument found");
-} else{
-    console.log("Argument found")
+
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
