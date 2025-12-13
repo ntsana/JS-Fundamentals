7e844897b57887a7cae946281545572245f54722
Ntsana Pheko <ntsanapheko@gmail.com>
2025-12-13 18:12:55 +0200

    corrections

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 let sentences = ["C is fun", "Python is cool", "JavaScript is amazing"]; 
 
-for(let i = 0; i < sentences.length; i++ ){
+for (let i = 0; i < sentences.length; i++ ){
     console.log(sentences[i]); 
 }
