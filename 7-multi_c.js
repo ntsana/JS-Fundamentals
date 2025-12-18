5e4a5383c4fa275caefce4ceacd00b634c576c07
Ntsana Pheko <ntsanapheko@gmail.com>
2025-12-18 13:17:19 +0200

    C is fun

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const x = Number(process.argv[2]); 
+
+if(Number.isNaN(x)){
+    console.log("Missing number of occurances. "); 
+}else{
+    let i = 0; 
+
+    while (i <x){
+        console.log("C is fun"); 
+        i++; 
+    }
+}
